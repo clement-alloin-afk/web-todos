51e66053f3fa0b4caa4b7c9d42b5d87d7b44c8f7
Clement <clement.alloin@student.hers.be>
2021-01-09 20:54:25 +0100

    petit pas chaloupé

--- frontend/src/axios-api.js
+++ frontend/src/axios-api.js
@@ -1,7 +1,7 @@
   import axios from 'axios'
 
 const getAPI = axios.create({
-    baseURL: 'http://127.0.0.1:8000/api',
+    baseURL: 'http://calloin.labo.infohers.org:8000/api',
     timeout: 1000,
 })
 
--- script_vps.sh
+++ script_vps.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
+cd ..
+sudo su # Dans le root ou pas ?
+
+#Changement de port
+sed -i "s/^Port.*/Port /" /etc/ssh/sshd_config
+systemctl restart sshd.service
+
+exit
+
+sudo apt-get update
+
 # Install fail2ban + Config
 sudo apt-get install fail2ban
 
@@ -13,9 +24,13 @@ iptables -A INPUT -i lo -j ACCEPT
 iptables -A INPUT -p tcp --dport 22222 -j ACCEPT
 iptables -A INPUT -p tcp ! --syn -m state --state NEW -j DROP
 iptables -A INPUT -m state --state ESTABLISHED,RELATED -j ACCEPT
+iptables -P INPUT DROP
 
 
-# Install Docker
-sudo apt install docker
+# Install Docker-compose
 sudo curl -L "https://github.com/docker/compose/releases/download/1.27.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-sudo chmod +x /usr/local/bin/docker-compose
+
+
+cd web-todos/
+sudo chmod +x /usr/local/bin/docker-compose;
+docker-compose up --build

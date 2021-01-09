72c231e1df34b07fc30ab0bf8318468a3d0af001
Clement <clement.alloin@student.hers.be>
2021-01-09 21:33:32 +0100

    petit changement v2

--- script_vps.sh
+++ script_vps.sh
@@ -1,13 +1,10 @@
 #!/bin/bash
 
 cd ..
-sudo su # Dans le root ou pas ?
 
 #Changement de port
-sed -i "s/^Port.*/Port 2222/" /etc/ssh/sshd_config
-systemctl restart sshd.service
-
-exit
+sudo sed -i "s/^Port.*/Port 2222/" /etc/ssh/sshd_config
+sudo systemctl restart sshd.service
 
 sudo apt-get update
 
@@ -15,22 +12,22 @@ sudo apt-get update
 sudo apt-get install fail2ban
 
 
-sed -i "s/^findtime.*/findtime = 600/" /etc/fail2ban/jail.conf
-sed -i "s/^maxretry.*/maxretry = 3/" /etc/fail2ban/jail.conf
+sudo sed -i "s/^findtime.*/findtime = 600/" /etc/fail2ban/jail.conf
+sudo sed -i "s/^maxretry.*/maxretry = 3/" /etc/fail2ban/jail.conf
 
 
 # Modif IpTables
-iptables -A INPUT -i lo -j ACCEPT
-iptables -A INPUT -p tcp --dport 22222 -j ACCEPT
-iptables -A INPUT -p tcp ! --syn -m state --state NEW -j DROP
-iptables -A INPUT -m state --state ESTABLISHED,RELATED -j ACCEPT
-iptables -P INPUT DROP
+sudo iptables -A INPUT -i lo -j ACCEPT
+sudo iptables -A INPUT -p tcp --dport 22222 -j ACCEPT
+sudo iptables -A INPUT -p tcp ! --syn -m state --state NEW -j DROP
+sudo iptables -A INPUT -m state --state ESTABLISHED,RELATED -j ACCEPT
+sudo iptables -P INPUT DROP
 
 
 # Install Docker-compose
 sudo curl -L "https://github.com/docker/compose/releases/download/1.27.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 
 
-cd web-todos/
+sudo cd web-todos/
 sudo chmod +x /usr/local/bin/docker-compose;
-docker-compose up --build
+sudo docker-compose up --build
